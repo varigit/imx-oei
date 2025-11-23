9185c55a967658c2fc6dd5c64210417057df6dce
Alexander Herfurtner <alexander.h@variscite.com>
2025-11-24 00:13:36 +0100

    ddr: Print DDR configuration
    Currently, OEI for DART-MX95 supports multiple DDR configurations,
    selected statically via DDR_CONFIG.
    
    To make it easier to understand which DDR configuration is built
    into the OEI, print it at startup.
    
    Signed-off-by: Alexander Herfurtner <alexander.h@variscite.com>
    (cherry picked from commit 59f7d25051cf59a05bfe979760409bf628d2a0ef)
    (cherry picked from commit 0f811ee86d92baaf0d9e293387f09db0f8f0cf40)

--- oei/ddr/main.c
+++ oei/ddr/main.c
@@ -102,6 +102,9 @@ int oei_main(uint32_t argc, uint32_t *argv)
         OEI_BUILD, OEI_COMMIT, OEI_DATE, OEI_TIME);
 
     printf("DDR OEI: SOC %s, Board %s\n", OEI_DEVICES, OEI_BOARD);
+#ifdef OEI_DDR_CONFIG
+    printf("DDR OEI: CFG: %s\n", OEI_DDR_CONFIG);
+#endif
     /**
      * Pass offset = 0 for iMX95 A0 since there is no ROM support
      * for training data dummy entry
